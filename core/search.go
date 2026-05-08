b86d876198940d71c2c133c6316857ad22f1e8eb
audderall <155036500+audderall@users.noreply.github.com>
2026-05-08 17:24:47 +0530

    correct path

--- core/search.go
+++ core/search.go
@@ -3,24 +3,33 @@ package core
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
 )
 
-func Search(term string) {
+func Search(term string) (string, error) {
 	encoded_term := url.QueryEscape(term)
-	url := fmt.Sprintf("https://youtube.com/?search_query=%s", encoded_term)
-	resp, err := http.Get(url)
+	url := fmt.Sprintf("https://youtube.com/results?search_query=%s", encoded_term)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36")
+	req.Header.Set("sec-ch-ua-arch", "x86")
+	req.Header.Set("sec-ch-ua-platform", "Linux")
+	req.Header.Set("Referer", "https://www.youtube.com/")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	fmt.Println(string(body))
+	return string(body), nil
 }
