5dfc23d62df7b3ce1ecd2570a37f7234be5eea0d
audderall <155036500+audderall@users.noreply.github.com>
2026-05-09 08:14:06 +0530

    docs: commit readme

---
+++ core/fetch.go
@@ -0,0 +1,11 @@
+package core
+
+//TODO
+//
+// func FetchVideo(videoID string) (Video, error) {
+// 	// Implementation for fetching a single video
+// }
+
+// func FetchPlaylist(playlistID string) (Playlist, error) {
+// 	// Implementation for fetching a playlist
+// }
